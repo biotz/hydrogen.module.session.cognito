f99f4e59a297a2021ca144779a2cb4d1f8378f51
Damian Hryniewicz <damian@magnet.coop>
2019-03-18 15:28:19 +0100

    set proper url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hydrogen/module.cljs "0.1.0"
   :description "Duct modules for doing cljs-based SPA applications the Hydrogen way."
-  :url "https://fix.me"
+  :url "https://github.com/magnetcoop/hydrogen.module.cljs"
   :license {:name "Mozilla Public Licence 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
